94ce9f7a641e117375fed1adbf2ddddf258fee06
AyumiUchimura <73804911+AyumiUchimura@users.noreply.github.com>
2021-04-10 18:57:38 +0900

    first commit for server

---
+++ cleaning_rota_server/src/main/java/tokyo/ammy/cleaning_rota_server/CleaningRotaServerApplication.java
@@ -0,0 +1,13 @@
+package tokyo.ammy.cleaning_rota_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleaningRotaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleaningRotaServerApplication.class, args);
+	}
+
+}
---
+++ cleaning_rota_server/src/test/java/tokyo/ammy/cleaning_rota_server/CleaningRotaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tokyo.ammy.cleaning_rota_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleaningRotaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
